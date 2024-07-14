416c4d41881524fa4d4f76854928ab0e506ed38a
hugonscm <hugo.nscm@outlook.com.br>
2024-07-14 10:39:20 -0300

    mudança UI

--- app/src/main/java/com/happs/tempo/ui/theme/Color.kt
+++ app/src/main/java/com/happs/tempo/ui/theme/Color.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.Color
 val bg_top_light = Color(0xFF5B4694)
 val bg_bottom_light = Color(0xFF623E6F)
 
-val bg_top_dark = Color(0xFF38129E)
-val bg_bottom_dark = Color(0xFF4F0C68)
+val bg_top_dark = Color(0xFF281E41)
+val bg_bottom_dark = Color(0xFF1E1322)
